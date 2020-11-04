7c70c0617260e9bfe89c23ce036d6f90f5dbc968
iamllcoolray <llcoolray24@gmail.com>
2020-11-03 21:07:33 -0500

    can get and print symbol

--- oolong/main.py
+++ oolong/main.py
@@ -1,4 +1,12 @@
 from core.version import get_version
+import argparse
 
 VERSION = get_version()
-print(VERSION)
+
+parser = argparse.ArgumentParser(prog="oolong", description="Oolong is a command-line interface that will output the stock qoute of the given symbol")
+
+parser.add_argument('-v', '--version', action='version', help='Displays Oolong\'s version number', version='Oolong - version '+VERSION)
+parser.add_argument('-q', '--quote', action='store', help='Displays the stock quote of a given symbol', type=str)
+
+args = parser.parse_args()
+print(args.quote)

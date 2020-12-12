24422fc33b2dd1dc4163b49a97b1ea65044499d7
iamllcoolray <llcoolray24@gmail.com>
2020-12-11 19:22:11 -0500

    added error handling for json responses

--- oolong/data/stockdata.py
+++ oolong/data/stockdata.py
@@ -7,4 +7,13 @@ class Quote:
     def stock_data(self):
         url = requests.get("https://query1.finance.yahoo.com/v7/finance/quote?symbols=" + self.symbol)
         data = url.json()
-        print(data)
+
+        try:
+            jsonResponse = data
+            print("Entire JSON response")
+            print(jsonResponse["quoteResponse"]["result"][0])
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')

82999b0fe260bda2731bf35d4cad262052f5110a
iamllcoolray <llcoolray24@gmail.com>
2020-12-25 00:41:57 -0500

    updated usage

--- oolong/main.py
+++ oolong/main.py
@@ -7,7 +7,7 @@ VERSION = get_version()
 parser = argparse.ArgumentParser(prog="oolong", description="Oolong is a command-line application that will output the stock qoute of the given symbol")
 
 parser.add_argument('-v', '--version', action='version', help='displays Oolong\'s version number', version='Oolong - version '+VERSION)
-parser.add_argument('-q', '--quote', action='store', help='displays the stock quote of a given symbol', type=str, nargs='?')
+parser.add_argument('-q', '--quote', action='store', help='displays the stock quote of a given symbol', type=str, nargs='?', metavar='stock symbol')
 
 args = parser.parse_args()
 

dce70d2986ff2bdcb9337871788f303691524d34
iamllcoolray <llcoolray24@gmail.com>
2021-04-01 22:21:14 -0400

    added news flags

--- grizzly/main.py
+++ grizzly/main.py
@@ -1,19 +1,21 @@
 from core.version import get_version
 from data.stockdata import stock_data
 import argparse
+import webbrowser
 
 VERSION = get_version()
 
 parser = argparse.ArgumentParser(prog="grizzly", description="Grizzly is a command-line application that will output the stock qoute of the given symbol")
 
-parser.add_argument('-v', '--version', action='version', help='displays Grizzly\'s version number', version='Grizzly - version '+VERSION)
+parser.add_argument('-v', '--version', action='version', help='displays %(prog)s\'s version number', version='%(prog)s - version '+VERSION)
 parser.add_argument('-q', '--quote', action='store', help='displays the stock quote of a given symbol', type=str, nargs='?', metavar='symbol')
+parser.add_argument('-n', '--news', action='store', help='displays the stock quote of a given symbol', nargs='?', metavar='symbol')
 
 args = parser.parse_args()
 
-if (args.quote is None):
-    args.quote = ["msft", "ibm", "wm", "ko", "emr", "brk-a", "brk-b", "wmt", "aapl", "hrl", "gpc", "jnj", "ba", "bac", "pnc", "wfc", "low", "xom", "pep", "axp", "cvs", "mmm", "cvx", "csco", "orcl", "v", "ma", "mcd", "afl", "wba", "atvi", "cmcsa", "qcom", "intc", "msi", "nvda", "sbux", "t", "vz", "txn", "unp"]
-    for i in args.quote:
-        stock_data(i)
-else:
+if (args.quote):
     stock_data(args.quote)
+
+if (args.news):
+    webbrowser.open_new_tab('https://finance.yahoo.com/quote/'+args.news+'/?p='+args.news)
+    webbrowser.open_new_tab('https://www.marketwatch.com/investing/stock/'+args.news+'?mod=quote_search')

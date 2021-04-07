47ecdfd15d696994e15e1eb60e7cb197e6e12b52
iamllcoolray <llcoolray24@gmail.com>
2021-04-06 23:42:33 -0400

    no args allowed => news.pm

--- lib/Grizzly/Command/news.pm
+++ lib/Grizzly/Command/news.pm
@@ -3,13 +3,13 @@ use Grizzly -command;
 use strict;
 use warnings;
 
-sub abstract { "display stock news" }
+sub abstract { "display stock market news" }
 
-sub description { "Display current news on a stock." }
+sub description { "Display current stock market news." }
 
 sub validate_args {
   my ($self, $opt, $args) = @_;
-  $self->usage_error("Need a symbol args") unless @$args;
+  $self->usage_error("No args allowed") if @$args;
 }
 
 sub execute {
